6d4b540ea97bccb30dcaa6fe4df88daf2c9167b9
Edgar Mejia <edgarinhm@hotmail.com>
2025-02-10 11:40:15 -0500

    add postions api routes add positions request

--- src/common/components/api-routes.js
+++ src/common/components/api-routes.js
@@ -1,3 +1,13 @@
 export const DivisionUrl = {
     get:()=>'/division'
 }
+
+export const PositionsUrl = {
+    get:()=>'/positions',
+    post:()=>'/positions',
+}
+
+export const PositionsByIdUrl = {
+    get:(positionId)=>`/positions/${positionId}`,
+    delete:(positionId)=>`/positions/${positionId}`,
+}
